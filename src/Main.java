9b3dd0fca2d7abdcd9f40c8cf5da7cdf3f38ab86
Rykard <rickygalante@hotmail.it>
2023-11-29 12:29:51 +0100

    esercizio costruttore

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,14 @@
 public class Main {
+    public static void main(String[] args) {
+        Studente studente= new Studente("Riccardo","Galante",247654921);
+
+    }
 }
+
+
+
+
+
+//    Scrivere un programma che contenga una classe che definisce uno Studente che abbia come proprietà nome, cognome ed identificativo.
+//        Per questa classe saranno necessari i metodi accessori (getters e setters) ed un costruttore.
+//        Nel main dichiarare un oggetto di tipo Studente.
